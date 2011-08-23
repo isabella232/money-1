f12ac28f0f1369c5f5c663cfff80d9cab7f73754
moepstaR <sebastianthobe@googlemail.com>
2011-08-23 18:06:20 +0200

    finally finished

--- lib/money/core_extensions.rb
+++ lib/money/core_extensions.rb
@@ -14,11 +14,9 @@ class String
     return Money.new(nil) if self.nil?
     if self.scan(/[a-zA-Z\!\"\§\$\%\&\/\(\)\=\?\*\’\ä\Ä\ö\Ö\ü\Ü\#\'\;\:\_\>\<\^\°\+]/).count == 0
       money = self.gsub(",",".")
-      if money.scan(".").count == 0
-        return Money.new(money * 100)
-      end
-      if money.split(".").count == 2 && money.scan(/[-]/).count <= 1
-        return Money.new((money.to_f * 100).to_i)
+      if money.scan(/[-]/).count < 2 
+        return Money.new(nil) if money.scan(/[-]/).count == 1 && !money.starts_with? "-"
+        return Money.new(money.to_f * 100)
       end
     end
     Money.new(nil)
