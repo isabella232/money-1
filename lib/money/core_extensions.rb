9bd28bd627d357c810f9d0c2ff0a2d7789f71c57
moepstaR <sebastianthobe@googlemail.com>
2011-08-23 15:56:19 +0200

    fix

--- lib/money/core_extensions.rb
+++ lib/money/core_extensions.rb
@@ -10,14 +10,6 @@ class Numeric
 end
 
 class String
-  # Parses the current string and converts it to a Money object.
-  # Excess characters will be discarded.
-  #
-  #   '100'.to_money       # => #<Money @cents=10000>
-  #   '100.37'.to_money    # => #<Money @cents=10037>
-  #   '100 USD'.to_money   # => #<Money @cents=10000, @currency="USD">
-  #   'USD 100'.to_money   # => #<Money @cents=10000, @currency="USD">
-  #   '$100 USD'.to_money   # => #<Money @cents=10000, @currency="USD">
   def to_money
     return Money.new(nil) if self.nil?
     if self.scan(/[a-zA-Z\!\"\§\$\%\&\/\(\)\=\?\*\’\ä\Ä\ö\Ö\ü\Ü\#\'\;\:\_\>\<\^\°\+]/).count == 0
@@ -25,7 +17,7 @@ class String
       if money.scan(".").count == 0
         Money.new(money * 100)
       end
-      if money.split(".").count == 2 && self.scan(/[-]/).count <= 1
+      if money.split(".").count == 2 && money.scan(/[-]/).count <= 1
         Money.new((money.to_f * 100).to_i)
       end
     end
